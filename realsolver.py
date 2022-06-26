29ec7ba1fd5c4423c7addb2890292daa8a90eb22
mosyawe1 <yawe.m@northeastern.edu>
2022-06-25 20:44:06 -0400

    Sudoku Solver Generates Grid and Solves

--- realsolver.py
+++ realsolver.py
@@ -1,8 +1,63 @@
-# Program that Generates and Solves Sudokus
+# Program that Generates and Solves Sudoku Puzzles
 import random
 import copy
 
+# Find empty parts of grid
+def empty(gr):
+    '''
+    Finds the empty spots on the grid which are represented by 0 and returns them as none
+    :param gr: a list
+    :return: returns 0's, if there is no blank squares it will return none
+    '''
+    for i, row in enumerate(gr):
+        for j, digit in enumerate(row):
+            if digit == 0:
+                return (i,j)  # row, col
+    return None
 
+# Display Grid
+def disp_grid(gr):
+    '''
+    Function that displays sudoku puzzle as matrix
+    :param gr: a list
+    :return: a matrix
+    '''
+    for i in range(len(gr)):
+        for j in range(len(gr[0])):
+            print(gr[i][j], end=' ')
+        print()
+
+# Check to see if the board is valid (grid, number, position)
+def valid(gr, num, loc):
+    '''
+    Function that checks to see if a sudoku puzzle is valid
+    :param gr: a list
+    :param num: integer that's added to a row
+    :param loc: integer that's used to represent the location of a number in the grid
+    :return: True if it's a valid position, False if not a valid position
+    '''
+    # Check row
+    for i in range(9):
+        if gr[loc[0]][i] == num:
+            return False
+    # Check column
+    for i in range(9):
+        if gr[i][loc[1]] == num:
+            return False
+
+    # Check 3 x 3 square
+    # Given a position it finds the square that the number is in
+    square_x = loc[1] // 3
+    square_y = loc[0] // 3
+    # Makes sure that the number doesn't appear twice in box
+    # Multiply by 3 to get the different indexes
+    for i in range(square_y*3,square_y*3+3):
+        for j in range(square_x*3,square_x*3+3):
+            if gr[i][j] == num and (i, j) != loc:
+                return False
+    return True
+
+#Sudoku Solver
 def solve(gr, rows=-1, cols=-1, nums=-1):
     '''
     Function that solves sudoku puzzles
@@ -35,6 +90,7 @@ def solve(gr, rows=-1, cols=-1, nums=-1):
 
     return False
 
+#Sudoku Generator
 def generate_puzzle():
     '''
     Function that creates a sudoku puzzle with random
@@ -60,62 +116,7 @@ def generate_puzzle():
                 break
     return Grid
 
-
-def disp_grid(gr): #display grid
-    '''
-    Function that displays sudoku puzzle as matrix
-    :param gr: a list
-    :return: a matrix
-    '''
-    for i in range(len(gr)):
-        for j in range(len(gr[0])):
-            print(gr[i][j], end=' ')
-        print()
-# Check to see if the board is valid (grid, number, position)
-def valid(gr, num, loc):
-    '''
-    Function that checks to see if a sudoku puzzle is valid
-    :param gr: a list
-    :param num: integer that's added to a row
-    :param loc: integer that's used to represent the location of a number in the grid
-    :return: True if it's a valid position, False if not a valid position
-    '''
-    # Check row
-    for i in range(9):
-        if gr[loc[0]][i] == num:
-            return False
-    # Check column
-    for i in range(9):
-        if gr[i][loc[1]] == num:
-            return False
-
-    # Check 3 x 3 square
-    # Given a position it finds the square that the number is in
-    square_x = loc[1] // 3
-    square_y = loc[0] // 3
-    # Makes sure that the number doesn't appear twice in box
-    # Multiply by 3 to get the different indexes
-    for i in range(square_y*3,square_y*3+3):
-        for j in range(square_x*3,square_x*3+3):
-            if gr[i][j] == num and (i, j) != loc:
-                return False
-    return True
-
-
-
-def empty(gr): #find empty parts of grid
-    '''
-    Finds the empty spots on the grid which are represented by 0 and returns them as none
-    :param gr: a list
-    :return: returns 0's, if there is no blank squares it will return none
-    '''
-    for i, row in enumerate(gr):
-        for j, digit in enumerate(row):
-            if digit == 0:
-                return (i,j)  # row, col
-    return None
-
-
+#Menu for sudoku game
 def menu():
     '''
     Creates a menu for solving and generating a sudoku
@@ -128,10 +129,10 @@ def menu():
         if make_puzzle == 1:
             puzzle = generate_puzzle()
             disp_grid(puzzle)
-            sol = int(input('\n To see the solved sudoku enter 2: \n'
-                            ' To exit the program enter 3: '))
+            sol = int(input('\n''To see the solved sudoku enter 2: \n'
+                            'To exit the program enter 3: '))
             if sol == 2:
-                solve(puzzle,-1,-1,-1)
+                solve(puzzle, -1, -1, -1)
                 disp_grid(puzzle)
             elif sol == 3:
                 print('Goodbye')
