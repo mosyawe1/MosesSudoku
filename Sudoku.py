7d417016ac2ff255650a3d54afc1d6c6dd7dc632
mosyawe1 <yawe.m@northeastern.edu>
2023-11-23 03:13:20 -0500

    Sudoku Solver Generates Grid and Solves

--- Sudoku.py
+++ Sudoku.py
@@ -6,6 +6,10 @@ class SudokuSolver:
         self.grid = [[0 for _ in range(9)] for _ in range(9)]
 
     def empty(self):
+        '''
+        Finds the empty spots on the grid which are represented by 0 and returns them as none
+        :return: (int, int) or None
+        '''
         for i, row in enumerate(self.grid):
             for j, digit in enumerate(row):
                 if digit == 0:
@@ -13,19 +17,36 @@ class SudokuSolver:
         return None
 
     def display_grid(self):
+        '''
+        Function that displays sudoku puzzle as matrix
+        '''
         for i in range(len(self.grid)):
             for j in range(len(self.grid[0])):
                 print(self.grid[i][j], end=' ')
             print()
 
     def valid(self, num, loc):
+        '''
+        Function that checks to see if a sudoku puzzle is valid
+        :param num: integer that's added to a row
+        :param loc: integer that's used to represent the location of a number in the grid
+        :return: True if it's a valid position, False if not a valid position
+        '''
+        # Check row
         for i in range(9):
-            if self.grid[loc[0]][i] == num or self.grid[i][loc[1]] == num:
+            if self.grid[loc[0]][i] == num:
+                return False
+        # Check column
+        for i in range(9):
+            if self.grid[i][loc[1]] == num:
                 return False
 
+        # Check 3 x 3 square
+        # Given a position it finds the square that the number is in
         square_x = loc[1] // 3
         square_y = loc[0] // 3
-
+        # Makes sure that the number doesn't appear twice in the box
+        # Multiply by 3 to get the different indexes
         for i in range(square_y * 3, square_y * 3 + 3):
             for j in range(square_x * 3, square_x * 3 + 3):
                 if self.grid[i][j] == num and (i, j) != loc:
@@ -33,6 +54,15 @@ class SudokuSolver:
         return True
 
     def solve(self, rows=-1, cols=-1, nums=-1):
+        '''
+        Function that solves sudoku puzzles
+        :param rows: integer: -1
+        :param cols: integer: -1
+        :param nums: integer: -1
+        :return: False if none of the numbers are valid, return True if they are valid
+        '''
+        # Picks an empty point, tries all numbers that work until it's valid
+        # Repeats until the Grid is full
         find = self.empty()
         lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         random.shuffle(lst)
@@ -47,23 +77,37 @@ class SudokuSolver:
 
                 if self.solve(rows, cols, nums):
                     return True
-
+                # If solve doesn't work, it changes the value to 0 and repeats the process recursively
                 self.grid[row][col] = 0
 
         return False
 
     def generate_puzzle(self):
+        '''
+        Function that creates a sudoku puzzle with random points marked as 0
+        '''
+        lst = list(range(0, 81))
+        random.shuffle(lst)
         self.solve()
 
-        puzzle_grid = copy.deepcopy(self.grid)
-
-        for _ in range(40):
-            row, col = random.randint(0, 8), random.randint(0, 8)
-            puzzle_grid[row][col] = 0
-
-        self.grid = puzzle_grid
+        count = 0
+        for i in lst:
+            row = i // 9
+            col = i % 9
+            new_grid = copy.deepcopy(self.grid)
+            num = new_grid[row][col]
+            new_grid[row][col] = 0
+            solved_grid = self.solve(new_grid, row, col, num)
+            if solved_grid is False:
+                self.grid[row][col] = 0
+                count += 1
+                if count == 25:
+                    break
 
     def menu(self):
+        '''
+        Creates a menu for solving and generating a sudoku
+        '''
         ans = True
         while ans:
             make_puzzle = int(input('Enter 1 to generate a sudoku:\n'
---
+++ Sudoku_Game.py
@@ -0,0 +1,55 @@
+import tkinter as tk
+from Sudoku import SudokuSolver
+
+
+class SudokuGame:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Sudoku")
+
+        self.sudoku_solver = SudokuSolver()
+        self.difficulty = "easy"  # Default difficulty
+
+        self.create_widgets()
+
+    def create_widgets(self):
+        # Create a Sudoku grid (entry widgets for the user input)
+        self.entries = [[tk.Entry(self.master, width=2, font=("Arial", 16))
+                         for _ in range(9)] for _ in range(9)]
+
+        for i in range(9):
+            for j in range(9):
+                self.entries[i][j].grid(row=i, column=j)
+                self.entries[i][j].bind('<Key>', lambda event, row=i, col=j: self.validate_input(event, row, col))
+
+                tk.Button(self.master, text="New Puzzle", command=self.generate_puzzle).grid(row=10, column=0)
+                tk.Button(self.master, text="Check Solution", command=self.check_solution).grid(row=10, column=1)
+                tk.Button(self.master, text="Easy", command=lambda: self.set_difficulty("easy")).grid(row=10, column=2)
+                tk.Button(self.master, text="Medium", command=lambda: self.set_difficulty("medium")).grid(row=10,
+                                                                                                          column=3)
+                tk.Button(self.master, text="Hard", command=lambda: self.set_difficulty("hard")).grid(row=10, column=4)
+
+    def generate_puzzle(self):
+        self.sudoku_solver.generate_puzzle(difficulty=self.difficulty)
+        self.update_gui()
+
+    def check_solution(self):
+        # Implement logic to check if the user's solution is correct
+        pass
+
+    def set_difficulty(self, difficulty):
+        self.difficulty = difficulty
+
+    def update_gui(self):
+        # Update the GUI based on the current state of the Sudoku grid
+        pass
+
+    def validate_input(self, event, row, col):
+        # Implement validation logic for user input
+        pass
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    game = SudokuGame(root)
+    root.mainloop()
